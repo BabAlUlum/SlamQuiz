e27ab01662459d5d7d45a16d851dabe80201e8fa
thiosey <thiosey@ndlp.fr>
2019-10-10 11:24:32 +0200

    [Core] Default .env file

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -4,6 +4,9 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Category;
+
+#
 
 class CategoryFixtures extends Fixture
 {
@@ -11,6 +14,24 @@ class CategoryFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        // POO
+        $category = new Category();
+        $category->setShortname('POO');
+        $category->setLongname('Programmation Orientée Objet');
+        $manager->persist($category);
+
+        // PHP
+        $category = new Category();
+        $category->setShortname('PHP');
+        $category->setLongname('Langage PHP');
+        $manager->persist($category);
+
+        // Symfony 4
+        $category = new Category();
+        $category->setShortname('Symf4');
+        $category->setLongname('Symfony version 4');
+        $manager->persist($category);
 
         $manager->flush();
     }
