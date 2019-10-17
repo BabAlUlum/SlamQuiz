73093f5798d72f76033e01d574bfaa03d7070bf6
thiosey <thiosey@ndlp.fr>
2019-10-17 12:54:22 +0200

    [Core] user

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
 
 
 class UserFixtures extends Fixture
@@ -18,14 +19,31 @@ class UserFixtures extends Fixture
     
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        
+        $user = new User();
+        $user->setEmail('thiosey@user.fr');
         $user->setPassword($this->passwordEncoder->encodePassword(
-                        thiosey,
-                        'Password123'
+                        $user,
+                        'azerty'
                         ));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setEmail('thiosey@superadmin.fr');
+        $user->setPassword($this->passwordEncoder->encodePassword(
+                        $user,
+                        'password'
+                        ));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setEmail('thiosey@admin.fr');
+        $user->setPassword($this->passwordEncoder->encodePassword(
+                        $user,
+                        'password123'
+                        ));
+        $manager->persist($user);
+
+
         $manager->flush();
     }
    
